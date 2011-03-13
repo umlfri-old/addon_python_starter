704faf7c90ad7abac70c15de9aff8891a2384528
janik <jan.janech@kst.uniza.sk>
2011-03-13 19:34:10 +0000

    - generating interface method bodies in python starter library - build shell script for python starter library

--- libraryTemplate/implementation/advProperty.py
+++ libraryTemplate/implementation/advProperty.py
@@ -1,3 +1,17 @@
 class AdvProperty(object):
     def __init__(self, name, haveIndex, documentation):
-        pass
+        self.__name = name
+        self.__haveIndex = haveIndex
+        self.documentation = documentation
+    
+    def getter(self, function):
+        self.__getter = function
+        return self
+    
+    def setter(self, function):
+        self.__setter = function
+        return self
+    
+    def indexer(self, function):
+        self.__indexer = function
+        return self
