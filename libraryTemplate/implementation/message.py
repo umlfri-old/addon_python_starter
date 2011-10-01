0d8bfc541f391d8cbfdfcccbb1b4c78bb46c7f3f
janik <jan.janech@kst.uniza.sk>
2011-10-01 17:40:08 +0000

    Message and MessageResult objects

--- libraryTemplate/implementation/message.py
+++ libraryTemplate/implementation/message.py
@@ -1,2 +1,69 @@
+from threading import Event
+
+from .communication import Server
+from .messageResult import MessageResult
+
 class Message(object):
-    pass
+    def __init__(self, objId, name):
+        self.__objId = objId
+        self.__name = name
+        self.__params = {}
+        self.__sendEvent = Event()
+        self.__result = None
+    
+    def boolean_parameter(self, name, value):
+        self.__params[name] = bool(value)
+        return self
+    
+    def int32_parameter(self, name, value):
+        self.__params[name] = int(value)
+        return self
+    
+    def float_parameter(self, name, value):
+        self.__params[name] = float(value)
+        return self
+    
+    def variant_parameter(self, name, value):
+        self.__params[name] = value
+        return self
+    
+    def string_parameter(self, name, value):
+        if not isinstance(value, basestring):
+            value = str(value)
+        self.__params[name] = value
+        return self
+    
+    def xy_parameter(self, name, value):
+        self.__params[name] = tuple(value)
+        return self
+    
+    def xywh_parameter(self, name, value):
+        self.__params[name] = tuple(value)
+        return self
+    
+    def wh_parameter(self, name, value):
+        self.__params[name] = tuple(value)
+        return self
+    
+    def keyvalue_parameter(self, name, value):
+        self.__params[name] = tuple(value)
+        return self
+    
+    def object_parameter(self, name, value):
+        self.__params[name] = value
+        return self
+    
+    def send(self):
+        Server.instance.send_command(self)
+        self.__sendEvent.wait()
+        return MessageResult(*self.__result)
+    
+    def send_async(self):
+        Server.instance.send_command_async(self)
+    
+    def create_message(self):
+        return 'exec', '%s.%s'%(self.__objId, self.__name), self.__params
+    
+    def accept(self, cmd, params):
+        self.__result = cmd, params
+        self.__sendEvent.set()
---
+++ libraryTemplate/implementation/messageResult.py
@@ -0,0 +1,105 @@
+from cStringIO import StringIO
+
+class MessageResult(object):
+    def __init__(self, cmd, params):
+        self.__cmd = cmd
+        self.__params = params
+    
+    def throws_exception(self, cmd, exception):
+        if cmd == self.__cmd:
+            raise exception(self.__params['params'])
+    
+    def return_void(self):
+        if self.__cmd is not None:
+            if 'params' in self.__params:
+                params = self.__params['params']
+            else:
+                params = None
+            raise Exception("Unknown exception %d: %r"%(self.__cmd, params))
+        
+    def return_boolean(self):
+        return bool(self.__params['result'])
+        
+    def return_inputstream(self):
+        ret = self.__params['result']
+        if not isinstance(ret, basestring):
+            ret = str(ret)
+        return StringIO(ret)
+    
+    def return_int32(self):
+        return int(self.__params['result'])
+    
+    def return_float(self):
+        return float(self.__params['result'])
+    
+    def return_variant(self):
+        return self.__params['result']
+    
+    def return_string(self):
+        ret = self.__params['result']
+        if not isinstance(ret, basestring):
+            ret = str(ret)
+        return ret
+    
+    def return_xy(self):
+        return tuple(self.__params['result'])
+    
+    def return_xywh(self):
+        return tuple(self.__params['result'])
+    
+    def return_wh(self):
+        return tuple(self.__params['result'])
+    
+    def return_keyvalue(self):
+        return tuple(self.__params['result'])
+    
+    def return_object(self):
+        return self.__params['result']
+        
+    def iterate_boolean(self):
+        for item in self.__params['result']:
+            yield bool(item)
+        
+    def iterate_inputstream(self):
+        for item in self.__params['result']:
+            if not isinstance(item, basestring):
+                item = str(item)
+            yield StringIO(item)
+    
+    def iterate_int32(self):
+        for item in self.__params['result']:
+            yield int(item)
+    
+    def iterate_float(self):
+        for item in self.__params['result']:
+            yield float(item)
+    
+    def iterate_variant(self):
+        for item in self.__params['result']:
+            yield item
+    
+    def iterate_string(self):
+        for item in self.__params['result']:
+            if not isinstance(item, basestring):
+                item = str(item)
+            yield item
+    
+    def iterate_xy(self):
+        for item in self.__params['result']:
+            yield tuple(item)
+    
+    def iterate_xywh(self):
+        for item in self.__params['result']:
+            yield tuple(item)
+    
+    def iterate_wh(self):
+        for item in self.__params['result']:
+            yield tuple(item)
+    
+    def iterate_keyvalue(self):
+        for item in self.__params['result']:
+            yield tuple(item)
+    
+    def iterate_object(self):
+        for item in self.__params['result']:
+            yield item
