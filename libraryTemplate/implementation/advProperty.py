abece11bbf21d757fc3dc543e80ad35e4539d2e1
janik <jan.janech@kst.uniza.sk>
2011-10-01 13:08:42 +0000

    AdvProperty finished

--- libraryTemplate/implementation/advProperty.py
+++ libraryTemplate/implementation/advProperty.py
@@ -1,8 +1,40 @@
+from .exceptions import ReadOnlyExceptions, NonIterableException
+
+class AdvPropertyIndexer(object):
+    def __init__(self, name, documentation, instance, getter, setter, iterator):
+        self.__name__ = name
+        self.__doc__ = documentation
+        
+        self.__instance = instance
+        self.__getter = getter
+        self.__setter = setter
+        self.__iterator = iterator
+    
+    def __getitem__(self, index):
+        return self.__getter(self.__instance, index)
+    
+    def __setitem__(self, index, value)
+        if self.__setter is None:
+            raise ReadOnlyException()
+        else:
+            self.__setter(self.__instance, index, value)
+    
+    def __iter__(self):
+        if self.__iterator is None:
+            raise NonIterableException()
+        else:
+            return iter(self.__iterator(self.__instance))
+
 class AdvProperty(object):
-    def __init__(self, name, haveIndex, documentation):
-        self.__name = name
-        self.__haveIndex = haveIndex
-        self.documentation = documentation
+    def __init__(self, name, hasIndex, documentation):
+        self.__name__ = name
+        self.__doc__ = documentation
+        
+        self.__hasIndex = hasIndex
+        
+        self.__getter = None
+        self.__setter = None
+        self.__iterator = None
     
     def getter(self, function):
         self.__getter = function
@@ -12,6 +44,20 @@ class AdvProperty(object):
         self.__setter = function
         return self
     
-    def indexer(self, function):
-        self.__indexer = function
+    def iterator(self, function):
+        self.__iterator = function
         return self
+    
+    def __get__(self, instance, cls = None):
+        if self.__hasIndex:
+            return AdvPropertyIndexer(self.__name__, self.__doc__, instance, self.__getter, self.__setter, self.__iterator)
+        elif self.__iterator is not None:
+            return iter(self.__iterator(instance))
+        else:
+            return self.__getter(instance)
+    
+    def __set__(self, instance, value):
+        if self.__hasIndex or self.__setter is None:
+            raise ReadOnlyException()
+        else:
+            self.__setter(instance, value)
---
+++ libraryTemplate/implementation/exceptions.py
@@ -0,0 +1,5 @@
+class ReadOnlyException(Exception):
+    pass
+
+class NonIterableException(Exception):
+    pass
