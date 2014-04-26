859bf47e40d29dd12021789ae7793b53886d66b6
janik <jan.janech@kst.uniza.sk>
2014-04-26 18:30:31 +0000

    python starter should work with new plugin system (when the system is programmed)

--- starter/pythonStarter.py
+++ starter/pythonStarter.py
@@ -1,5 +1,5 @@
+from itertools import chain
 from lib.Distconfig import IS_FROZEN
-from lib.Addons.Plugin.Communication.Medium import CPipeMedium
 from multiprocessing.forking import duplicate
 
 import os
@@ -29,7 +29,7 @@ class CPythonStarter(object):
     def __init__(self, plugin):
         self.__plugin = plugin
     
-    def Start(self):
+    def Start(self, channel):
         path = self.__plugin.GetPath()
         uri = self.__plugin.GetUri()
         env = os.environ.copy()
@@ -38,37 +38,32 @@ class CPythonStarter(object):
         env['UMLFRI_URI'] = str(uri)
         env['UMLFRI_PATH'] = str(path)
         
-        pin = os.pipe()
-        pout = os.pipe()
-        self.__plugin.GetPluginManager().NewConnection(CPipeMedium(pin[0], pout[1]), (pin[0], pout[1]))
-        
         if os.name == 'nt':
             import msvcrt
-            ppin = duplicate(msvcrt.get_osfhandle(pout[0]), inheritable=True)
-            ppout = duplicate(msvcrt.get_osfhandle(pin[1]), inheritable=True)
-            env['UMLFRI_PIN'] = str(ppin) 
+            ppin = duplicate(msvcrt.get_osfhandle(channel.GetReaderFD()), inheritable=True)
+            ppout = duplicate(msvcrt.get_osfhandle(channel.GetWriterFD()), inheritable=True)
+            env['UMLFRI_PIN'] = str(ppin)
             env['UMLFRI_POUT'] = str(ppout)
             ppin = msvcrt.open_osfhandle(ppin, os.O_RDONLY)
             ppout = msvcrt.open_osfhandle(ppout, os.O_APPEND)
             self.__process = subprocess.Popen(self.__pl_runner, close_fds = False, env = env)
-            os.close(pin[1])
-            os.close(pout[0])
+            channel.CloseOthers()
             os.close(ppin)
             os.close(ppout)
             
         else:
-            env['UMLFRI_PIN'] = str(pout[0])
-            env['UMLFRI_POUT'] = str(pin[1])
+            env['UMLFRI_PIN'] = str(channel.GetReaderFD())
+            env['UMLFRI_POUT'] = str(channel.GetWriterFD())
             pid = os.fork()
             if pid:
                 #parent
                 self.__pid = pid
+                channel.CloseOthers()
                 os.close(pin[1])
                 os.close(pout[0])
             else:
                 #child
-                os.close(pin[0])
-                os.close(pout[1])
+                channel.Close()
                 os.execve(self.__pl_runner[0], self.__pl_runner, env)
     
     def Terminate(self):
